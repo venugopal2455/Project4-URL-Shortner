83566263cc998366ccd803aa42f023056a90bd63
Zuber Khan <Zuberkhan034@gmail.com>
2022-05-17 17:59:19 +0530

    getUrl Handler added

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,16 +1,39 @@
 const urlModel = require("../models/urlModel")
 const mongoose = require('mongoose')
 
-const isValid = function(value) {
-    if (typeof value === 'undefined' || value === null) return false 
-    if (typeof value === 'string' && value.trim().length === 0) return false 
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
     return true;
 }
 
-const isValidRequestBody = function(requestBody) {
-    return Object.keys(requestBody).length > 0; 
+const isValidRequestBody = function (requestBody) {
+    return Object.keys(requestBody).length > 0;
 }
+// =========================================postUrl=======================================================
 
 
 
-// ======+===================================postUrl=======================================================
+// =========================================getUrl=======================================================
+
+const getUrl = async function (req, res) {
+    try {
+
+        const urlCode = req.params.urlCode
+        if (!urlCode) {
+            return res.status(400).send({ status: false, message: "Please enter urlCode!" });
+        }
+
+        const foundedUrl = await urlModel.findOne({ urlCode: urlCode })
+        if (!foundedUrl) {
+            return res.status(404).send({ status: false, message: "Url not found" })
+        }
+
+        const OriginalUrl = foundedUrl.longUrl;
+        return res.status(301).redirect(OriginalUrl)
+
+    } catch (err) {
+        return res.status(500).send({ status: false, error: err.message })
+    }
+}
+module.exports = {getUrl}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+
 const urlController=require('../controller/urlController')
 
-
-
+router.get("/:urlCode", urlController.getUrl);
 
 module.exports = router

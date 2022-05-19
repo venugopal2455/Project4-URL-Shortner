60e077c5064675962ca18b26b251ea70fcdea8a0
Zuber Khan <50729008+ZUBERKHAN034@users.noreply.github.com>
2022-05-19 15:02:27 +0530

    Project-4 done

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,4 +6,10 @@ const urlController=require('../controller/urlController')
 router.post("/url/shorten",urlController.shortUrl);
 router.get("/:urlCode", urlController.getUrl);
 
+
+// if api is invalid OR wrong URL
+router.all("/*", function (req, res) {
+    res.status(404).send({ status: false, msg: "The api you requested is not available" }) 
+});
+
 module.exports = router
